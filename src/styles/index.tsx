89774c2a4b9414526f3f281bf79780a7e5ad2afa
Daniel Toloto <dctoloto@gmail.com>
2021-03-30 23:27:29 -0300

    add: initial setup

---
+++ src/styles/index.tsx
@@ -0,0 +1,17 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+  *{
+    margin: 0;
+    padding: 0;
+    outline: 0;
+    box-sizing: border-box;
+    -webkit-font-smoothing: antialiased;
+    font-family: 'Montserrat', sans-serif;
+  }
+
+  a{
+    transition: .2s;
+    text-decoration: none;
+  }
+`;
---
+++ src/styles/theme.ts
@@ -0,0 +1,14 @@
+const theme = {
+  colors: {
+    primary: '#1DB954',
+    dark: '#191414',
+    mediumDark: '#181818',
+    lightDark: '#282828',
+    mediumLight: '#888090',
+    light: '#b3b3b3',
+  },
+  boxShadow: '0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22);',
+  borderRadius: '4px',
+};
+
+export default theme;
